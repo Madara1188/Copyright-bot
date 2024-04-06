6893ca11245ce138b768ef1ae02eab256c1fddc9
Madara <147428837+Madara1188@users.noreply.github.com>
2024-04-07 00:51:18 +0530

    Update main.py

--- COPYRIGHT2/modules/main.py
+++ COPYRIGHT2/modules/main.py
@@ -150,7 +150,7 @@ async def delete_and_reply(_, msg):
 # ------------------------------------------------------------------------------
 @app.on_message(filters.animation | filters.audio | filters.document | filters.photo | filters.sticker | filters.video)
 async def keep_reaction_message(client, message: Message):
-   
+   pass
 # -------------------------------
 async def delete_pdf_files(client, message):
     if message.document and message.document.mime_type == "application/pdf":
